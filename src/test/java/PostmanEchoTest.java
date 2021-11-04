cfc11e6c7078557b37149abce6f9847b6b936888
Klevtsov Andrey <andrey.tiger99@gmail.com>
2021-11-04 22:07:18 +0300

    initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostmanEchoTest {
+    @Test
+    public void ShouldReturnName() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Andrey") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Andrey1"))
+        ;
+    }
+}
