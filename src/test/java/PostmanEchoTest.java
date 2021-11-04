8c734ecffbd737170a9df5dfd792588d03f9c0f5
Klevtsov Andrey <andrey.tiger99@gmail.com>
2021-11-04 22:08:58 +0300

    fixed for success CI

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Andrey1"))
+                .body("data", equalTo("Andrey"))
         ;
     }
 }
